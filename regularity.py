bf9ae421b2c71873a48841e522340e5e12feb433
Jaehyun Ahn <jaehyunahn@nexon.co.kr>
2020-04-28 18:31:01 +0900

    Update regularity.py
    do not need to sum division.

--- regularity.py
+++ regularity.py
@@ -5,9 +5,6 @@ def regularity_df(df, boolean=False):
     # columns의 순서가 중요하다
     df_temp = df.copy()
     if boolean == True:
-    df_temp = df_temp.astype(bool)
+        df_temp = df_temp.astype(bool)
   
-    df_sum = df_temp.sum(axis=1)
-    frac = df_temp.div(df_sum.iloc[0], axis='columns')
-  
-    return entropy(frac.T)
+    return entropy(df_temp.T)
