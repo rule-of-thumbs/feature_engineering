6eb4f725fd2554d5c56ace821b47d3e2621d087b
Jaehyun Ahn <jaehyunahn@nexon.co.kr>
2020-04-28 13:23:28 +0900

    Update log_normalization.py

--- log_normalization.py
+++ log_normalization.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 
+# nobs, mx, mu, var, skew, kurt = stats.describe(log_transformed)
+# get log normalization fit parameters: Log-Max
+# https://towardsdatascience.com/transforming-skewed-data-73da4c2d0d16
 def log_normal_df(df):
   # convert log normalization
   
@@ -11,15 +14,18 @@ def log_normal_df(df):
     return df, None
   if df.loc[(df != 0) & (~pd.isnull(df))].max() == 0:
     return df, None
+  if df[df < 0].any():
+    # log(negative) returns np.nan
+    return df, None
   # get base
-  log_max = np.log(df.loc[(df != 0) & (df != np.nan)].max())
+  base = np.log(df.loc[(df != 0) & (~pd.isnull(df))].max())
   e = df.loc[(df != 0) & (~pd.isnull(df))].min() * 1e-1
   
   # only change e when it zero, fill na
   df_temp = df.copy()
   df_temp.replace(0, e, inplace=True)
-  df_temp.fillna(df_temp.mean(), inplace=True)
+  df_temp.fillna(df_temp.median(), inplace=True)
   
   # log transform
-  log_transformed = np.log(df_temp) / np.log(log_max)
-  return log_transformed, log_max
+  log_transformed = np.log(df_temp) / np.log(base)
+  return log_transformed, base
