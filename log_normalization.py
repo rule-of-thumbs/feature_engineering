64ce9c3ad200ecee894717394693e44791f156f9
Jaehyun Ahn <jaehyunahn@nexon.co.kr>
2020-04-28 12:50:43 +0900

    Update log_normalization.py
    change or condition to and

--- log_normalization.py
+++ log_normalization.py
@@ -1,25 +1,24 @@
 import numpy as np
 import pandas as pd
 
-# nobs, mx, mu, var, skew, kurt = stats.describe(log_transformed)
-# get log normalization fit parameters: Log-Max
-# https://towardsdatascience.com/transforming-skewed-data-73da4c2d0d16
 def log_normal_df(df):
   # convert log normalization
   
   if not df.dtype.kind in 'biufc':
     # bool int uint float
     return df, None
-  if pd.isnull(df).all():
+  if pd.isnull(df).any():
     return df, None
-  if df.max() == 0:
+  if df.loc[(df != 0) & (~pd.isnull(df))].max() == 0:
     return df, None
   # get base
-  log_max = np.log(df.loc[(df != 0) | (df != np.nan)].max())
-  e = df.loc[(df != 0) | (df != np.nan)].min() * 1e-1
+  log_max = np.log(df.loc[(df != 0) & (df != np.nan)].max())
+  e = df.loc[(df != 0) & (~pd.isnull(df))].min() * 1e-1
+  print(e)
   
   # only change e when it zero, fill na
-  df_temp = df.replace(0, e)
+  df_temp = df.copy()
+  df_temp.replace(0, e, inplace=True)
   df_temp.fillna(df_temp.mean(), inplace=True)
   
   # log transform
