d408a39f924cf8335313e06acdee03396ffd0a81
Jaehyun Ahn <jaehyunahn@nexon.co.kr>
2020-04-28 10:42:38 +0900

    Update log_normalization.py
    change comment

--- log_normalization.py
+++ log_normalization.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def log_normal_df(df):
+    # nobs, mx, mu, var, skew, kurt = stats.describe(log_transformed)
+    # get log normalization fit parameters: Log-Max
+    # https://towardsdatascience.com/transforming-skewed-data-73da4c2d0d16
     # convert df into log normalization df
     log_max = np.log(df.max())
     e = df.loc[df != 0].min() * 1e-1
