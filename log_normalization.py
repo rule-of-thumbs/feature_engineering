92876f2ddbae6419b93fdb7d105c78b35b2cb447
Jaehyun Ahn <jaehyunahn@nexon.co.kr>
2020-04-28 17:55:54 +0900

    Update log_normalization.py

--- log_normalization.py
+++ log_normalization.py
@@ -5,29 +5,29 @@ import pandas as pd
 # get log normalization fit parameters: Log-Max
 # https://towardsdatascience.com/transforming-skewed-data-73da4c2d0d16
 def log_normal_df(df):
-  # convert log normalization
+    # convert log normalization
   
-  if not df.dtype.kind in 'biufc':
-    # bool int uint float
-    return df, None
-  if pd.isnull(df).any():
-    return df, None
-  if df.loc[(df != 0) & (~pd.isnull(df))].max() == 0:
-    return df, None
-  if df[df < 0].any():
-    # log(negative) returns np.nan
-    return df, None
-  if (df == 0).all():
-    return df, None
-  # get base
-  e = df.loc[(df != 0) & (~pd.isnull(df))].min() * 1e-1
-  base = np.log(df.loc[(df != 0) & (~pd.isnull(df))].max()) + e
+    if not df.dtype.kind in 'biufc':
+      # bool int uint float
+      return df, None
+    if pd.isnull(df).any():
+      return df, None
+    if df.loc[(df != 0) & (~pd.isnull(df))].max() == 0:
+      return df, None
+    if df[df < 0].any():
+      # log(negative) returns np.nan
+      return df, None
+    if (df == 0).all():
+      return df, None
+    # get base
+    e = df.loc[(df != 0) & (~pd.isnull(df))].min() * 1e-1
+    base = np.log(df.loc[(df != 0) & (~pd.isnull(df))].max()) + e
   
-  # only change e when it zero, fill na
-  df_temp = df.copy()
-  df_temp.replace(0, e, inplace=True)
-  df_temp.fillna(df_temp.median(), inplace=True)
+    # only change e when it zero, fill na
+    df_temp = df.copy()
+    df_temp.replace(0, e, inplace=True)
+    df_temp.fillna(df_temp.median(), inplace=True)
   
-  # log transform
-  log_transformed = np.log(df_temp) / np.log(base)
-  return log_transformed, base
+    # log transform
+    log_transformed = np.log(df_temp) / np.log(base)
+    return log_transformed, base
