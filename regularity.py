dc2329b7934ab0d9e5d74a46d0646dff5decb42a
Jaehyun Ahn <jaehyunahn@nexon.co.kr>
2020-04-28 17:55:15 +0900

    Update regularity.py

--- regularity.py
+++ regularity.py
@@ -1,10 +1,13 @@
 from scipy.stats import entropy
 
 # df, sequence > bool(regular), amount(regular)
-def regularity_df(df, columns):
-    # columns의 순서 중요하다
+def regularity_df(df, columns, boolean=False):
+    # columns의 순서가 중요하다
     df_temp = df[columns].copy()
+    if boolean == True:
+    df_temp = df_temp.astype(bool)
+  
     df_sum = df_temp.sum(axis=1)
     frac = df_temp.div(df_sum.iloc[0], axis='columns')
+  
     return entropy(frac.T)
-
