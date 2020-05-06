91bdbfa08b67ccc9788a3fcbadb6ffa80bc0e862
Jaehyun Ahn <jaehyunahn@nexon.co.kr>
2020-05-06 09:30:27 +0900

    Update regularity.py
    add shift option

--- regularity.py
+++ regularity.py
@@ -3,14 +3,14 @@ import pandas as pd
 from scipy.stats import entropy
 
 # df, sequence > bool(regular), amount(regular)
-def regularity_df(df, boolean=False):
+def regularity_df(df, boolean=False, shift=False):
     # columns의 순서가 중요하다
     df_temp = df.copy()
     
     # replace nan values
     df_temp[(pd.isnull(df_temp))] = 0
     # parallel move when data is negative
-    if df_temp.min() < 0:
+    if (df_temp.min() < 0) and (shift):
         df_temp += abs(df_temp.min())
     
     if boolean == True:
